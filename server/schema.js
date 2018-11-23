893778e44cbe88619c4c06b2c68abdd29dee38f4
Eric Abenojar <abenoeric@gmail.com>
2018-11-22 20:16:30 -0600

    Testing different data in query for graphql

--- server/schema.js
+++ server/schema.js
@@ -14,23 +14,63 @@ const {
 const CoinType = new GraphQLObjectType({
   name: "Coin",
   fields: () => ({
-    name: { type: GraphQLString }
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    rocket: { type: RocketType }
+  })
+});
+
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString }
+  })
+});
+
+const MissionId = new GraphQLObjectType({
+  name: "MissionId",
+  fields: () => ({})
+});
+const MissionType = new GraphQLObjectType({
+  name: "Mission",
+  fields: () => ({
+    mission_name: { type: GraphQLString },
+    mission_id: { type: GraphQLString },
+    payload_ids: { type: GraphQLString }
   })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
-    coins: {
-      type: new GraphQLList(CoinType),
+    launches: {
+      type: CoinType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
       resolve(parent, args) {
         return axios
-          .get("https://api.coinmarketcap.com/v2/ticker/?limit=10")
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
           .then(res => {
-            console.log("DATAAA", res.data);
             return res.data;
           })
           .catch(err => console.log("ERROR", err));
+      }
+    },
+    mission: {
+      type: MissionType,
+      args: {
+        mission_id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/missions/${args.mission_id}`)
+          .then(res => {
+            console.log("MISSIONS", res.data);
+            return res.data;
+          })
+          .catch(err => console.log("Error"));
       }
     }
   }
